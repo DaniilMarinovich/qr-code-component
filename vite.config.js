b73c14a5a1323c25f24e104e14fbfc993b00cc16
DaniilMarinovich <marinovich.daniil@gmail.com>
2025-09-15 17:08:50 +0300

    build: deploy to docs for GitHub Pages 3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<repo-name>/',
-  build: { outDir: 'docs' },
-});
+  base: '/qr-code-component/',
+  build: { outDir: 'docs' }
+})
